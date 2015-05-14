0d8bfcdc3a5663668e5d05ef7b877474b420dcbd
Rain <ssiori@gmail.com>
2015-05-14 23:38:21 +0800

    info activity comment

--- app/src/main/scala/com/yue/anime/hacg/Article.scala
+++ app/src/main/scala/com/yue/anime/hacg/Article.scala
@@ -22,6 +22,24 @@ case class Tag(name: String, url: String) extends Parcelable {
   }
 }
 
+case class Comment(content: String, user: String, face: String, time: Option[Date]) extends Parcelable {
+  def this(e: Element) = {
+    this(e.select(".comment-content").text(),
+      e.select(".fn").text(),
+      e.select(".avatar").attr("abs:src"),
+      e.select("time").attr("datetime"))
+  }
+
+  override def describeContents() = 0
+
+  override def writeToParcel(dest: Parcel, flags: Int) {
+    dest.writeString(content)
+    dest.writeString(user)
+    dest.writeString(face)
+    dest.writeLong(if (time.nonEmpty) time.get.getTime else 0)
+  }
+}
+
 case class Article(title: String,
                    link: String,
                    image: String,
@@ -32,19 +50,19 @@ case class Article(title: String,
                    category: Option[Tag],
                    tags: List[Tag]) extends Parcelable {
 
-  def this(element: Element) = {
-    this(element.select("header a").text().trim,
-      element.select("header a").attr("abs:href"),
-      element.select(".entry-content img").attr("abs:src"),
-      element.select(".entry-content p,.entry-summary p").text().trim,
-      element.select("time").attr("datetime"),
-      element.select("header .comments-link").text().trim match {
+  def this(e: Element) = {
+    this(e.select("header a").text().trim,
+      e.select("header a").attr("abs:href"),
+      e.select(".entry-content img").attr("abs:src"),
+      e.select(".entry-content p,.entry-summary p").text().trim,
+      e.select("time").attr("datetime"),
+      e.select("header .comments-link").text().trim match {
         case str if str.matches( """^$\d+""") => str.toInt
         case _ => 0
       },
-      element.select("header .author a").take(1).map(e => new Tag(e)).headOption,
-      element.select("footer .cat-links a").take(1).map(e => new Tag(e)).headOption,
-      element.select("footer .tag-links a").map(o => new Tag(o)).toList)
+      e.select("header .author a").take(1).map(e => new Tag(e)).headOption,
+      e.select("footer .cat-links a").take(1).map(e => new Tag(e)).headOption,
+      e.select("footer .tag-links a").map(o => new Tag(o)).toList)
   }
 
 
@@ -72,6 +90,23 @@ object Tag {
     )
 
     override def newArray(size: Int): Array[Tag] = new Array[Tag](size)
+  }
+}
+
+object Comment {
+
+  val CREATOR: Parcelable.Creator[Comment] = new Parcelable.Creator[Comment] {
+    override def createFromParcel(source: Parcel): Comment = new Comment(
+      source.readString(),
+      source.readString(),
+      source.readString(),
+      source.readLong() match {
+        case 0 => None
+        case l => Option(new Date(l))
+      }
+    )
+
+    override def newArray(size: Int): Array[Comment] = new Array[Comment](size)
   }
 }
 
--- app/src/main/scala/com/yue/anime/hacg/InfoActivity.scala
+++ app/src/main/scala/com/yue/anime/hacg/InfoActivity.scala
@@ -2,13 +2,19 @@ package com.yue.anime.hacg
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.RecyclerView
-import android.view.View
+import android.support.v7.widget.RecyclerView.OnScrollListener
+import android.support.v7.widget.{LinearLayoutManager, RecyclerView}
+import android.view.{LayoutInflater, ViewGroup, View}
 import android.webkit.WebView
-import android.widget.ProgressBar
+import android.widget.{ImageView, TextView, ProgressBar}
+import com.mugen.{MugenCallbacks, Mugen}
 import com.squareup.okhttp.{OkHttpClient, Request}
+import com.squareup.picasso.Picasso
 import com.yue.anime.hacg.Common._
 import org.jsoup.Jsoup
+
+import scala.collection.JavaConversions._
+import scala.collection.mutable.ArrayBuffer
 
 /**
  * Info activity
@@ -19,10 +25,38 @@ class InfoActivity extends AppCompatActivity {
   lazy val web: WebView = findViewById(R.id.webview)
   lazy val progress: ProgressBar = findViewById(R.id.progress1)
   lazy val recycle: RecyclerView = findViewById(R.id.recycler)
+  lazy val adapter = new CommentAdapter
 
   def busy(b: Boolean): Unit = {
     progress.setVisibility(if (b) View.VISIBLE else View.INVISIBLE)
     progress.setIndeterminate(b)
+  }
+
+  class CommentHolder(val view: View) extends RecyclerView.ViewHolder(view) {
+    val context = view.getContext
+    val text1: TextView = view.findViewById(R.id.text1)
+    val text2: TextView = view.findViewById(R.id.text2)
+    val image: ImageView = view.findViewById(R.id.image1)
+  }
+
+  class CommentAdapter extends RecyclerView.Adapter[CommentHolder] {
+    val data = new ArrayBuffer[Comment]()
+
+    override def getItemCount: Int = data.size
+
+    override def onBindViewHolder(holder: CommentHolder, position: Int): Unit = {
+      val item = data(position)
+      holder.text1.setText(item.user)
+      holder.text2.setText(item.content)
+      if (item.face.isEmpty) {
+        holder.image.setImageResource(R.mipmap.ic_launcher)
+      } else {
+        Picasso.`with`(holder.context).load(item.face).placeholder(R.mipmap.ic_launcher).into(holder.image)
+      }
+    }
+
+    override def onCreateViewHolder(parent: ViewGroup, viewType: Int): CommentHolder =
+      new CommentHolder(LayoutInflater.from(parent.getContext).inflate(R.layout.comment_item, parent, false))
   }
 
   override def onCreate(savedInstanceState: Bundle): Unit = {
@@ -30,9 +64,21 @@ class InfoActivity extends AppCompatActivity {
     setContentView(R.layout.activity_info)
     setSupportActionBar(findViewById(R.id.toolbar))
     setTitle(article.title)
+
+    recycle.setLayoutManager(new LinearLayoutManager(this))
+    recycle.setAdapter(adapter)
+
+    //    recycle.setOnScrollListener(new RecyclerView.OnScrollListener {
+    //      override def onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int): Unit =
+    //        super.onScrolled(recyclerView, dx, dy)
+    //
+    //      override def onScrollStateChanged(recyclerView: RecyclerView, newState: Int): Unit =
+    //        super.onScrollStateChanged(recyclerView, newState)
+    //    })
+
     busy(b = true)
-    new ScalaTask[Void, Void, String] {
-      override def background(params: Void*): String = {
+    new ScalaTask[Void, Void, (String, List[Comment])] {
+      override def background(params: Void*): (String, List[Comment]) = {
         val http = new OkHttpClient()
         val request = new Request.Builder().get().url(article.link).build()
         val response = http.newCall(request).execute()
@@ -44,13 +90,18 @@ class InfoActivity extends AppCompatActivity {
           .replaceAll( """<a href="#">.*?</a>""", "")
           .replaceAll( """</?embed.*?>""", "")
         //          .replaceAll( """(?<!\w+=['"]?)(?:https?://|magnet:\?xt=)[^\s\"\'\<]+""", """<a href="$0">$0</a>""")
-        using(io.Source.fromInputStream(getResources.openRawResource(R.raw.template))) {
-          reader => reader.mkString.replace("{{title}}", article.title).replace("{{body}}", body)
-        }
+        Tuple2(
+          using(io.Source.fromInputStream(getResources.openRawResource(R.raw.template))) {
+            reader => reader.mkString.replace("{{title}}", article.title).replace("{{body}}", body)
+          },
+          dom.select("#comments .commentlist>li").map(e => new Comment(e)).toList
+        )
       }
 
-      override def post(html: String): Unit = {
-        web.loadDataWithBaseURL(article.link, html, "text/html", "UTF-8", null)
+      override def post(result: (String, List[Comment])): Unit = {
+        web.loadDataWithBaseURL(article.link, result._1, "text/html", "utf-8", null)
+        adapter.data.append(result._2: _*)
+        adapter.notifyDataSetChanged()
         busy(b = false)
       }
     }.execute()
