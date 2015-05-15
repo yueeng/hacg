844e9e135ed59e799aa348abc9b882fae4d64621
Rain <ssiori@gmail.com>
2015-05-15 15:19:25 +0800

    info activity comment load more

--- app/src/main/scala/com/yue/anime/hacg/InfoActivity.scala
+++ app/src/main/scala/com/yue/anime/hacg/InfoActivity.scala
@@ -2,12 +2,11 @@ package com.yue.anime.hacg
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.RecyclerView.OnScrollListener
 import android.support.v7.widget.{LinearLayoutManager, RecyclerView}
-import android.view.{LayoutInflater, ViewGroup, View}
+import android.util.Log
+import android.view.{LayoutInflater, View, ViewGroup}
 import android.webkit.WebView
-import android.widget.{ImageView, TextView, ProgressBar}
-import com.mugen.{MugenCallbacks, Mugen}
+import android.widget.{ImageView, ProgressBar, TextView}
 import com.squareup.okhttp.{OkHttpClient, Request}
 import com.squareup.picasso.Picasso
 import com.yue.anime.hacg.Common._
@@ -22,14 +21,27 @@ import scala.collection.mutable.ArrayBuffer
  */
 class InfoActivity extends AppCompatActivity {
   lazy val article = getIntent.getParcelableExtra[Article]("article")
-  lazy val web: WebView = findViewById(R.id.webview)
   lazy val progress: ProgressBar = findViewById(R.id.progress1)
-  lazy val recycle: RecyclerView = findViewById(R.id.recycler)
+  lazy val progress2: ProgressBar = findViewById(R.id.progress2)
   lazy val adapter = new CommentAdapter
 
-  def busy(b: Boolean): Unit = {
+  var _busy = false
+  var _busy2 = false
+
+  def busy = _busy
+
+  def busy_=(b: Boolean) {
+    _busy = b
     progress.setVisibility(if (b) View.VISIBLE else View.INVISIBLE)
     progress.setIndeterminate(b)
+  }
+
+  def busy2 = _busy2
+
+  def busy2_=(b: Boolean) {
+    _busy2 = b
+    progress2.setVisibility(if (b) View.VISIBLE else View.INVISIBLE)
+    progress2.setIndeterminate(b)
   }
 
   class CommentHolder(val view: View) extends RecyclerView.ViewHolder(view) {
@@ -59,51 +71,81 @@ class InfoActivity extends AppCompatActivity {
       new CommentHolder(LayoutInflater.from(parent.getContext).inflate(R.layout.comment_item, parent, false))
   }
 
+  def query(url: String, content: Boolean = false): Unit = {
+    Log.i("IA", "%s, %s".format(busy.toString, busy2.toString))
+    if (busy || busy2) {
+      return
+    }
+    busy = content
+    busy2 = true
+    new ScalaTask[Void, Void, (String, String, List[Comment])] {
+      override def background(params: Void*): (String, String, List[Comment]) = {
+        val http = new OkHttpClient()
+        val request = new Request.Builder().get().url(url).build()
+        val response = http.newCall(request).execute()
+        val html = response.body().string()
+        val dom = Jsoup.parse(html, url)
+
+        Tuple3(
+          if (content) using(io.Source.fromInputStream(getResources.openRawResource(R.raw.template))) {
+            reader => reader.mkString.replace("{{title}}", article.title).replace("{{body}}",
+              dom.select(".entry-content").html().replaceAll( """\s{0,1}style=".*?"""", "")
+                .replaceAll( """\s{0,1}class=".*?"""", "")
+                .replaceAll( """<a href="#">.*?</a>""", "")
+                .replaceAll( """</?embed.*?>""", ""))
+            //          .replaceAll( """(?<!\w+=['"]?)(?:https?://|magnet:\?xt=)[^\s\"\'\<]+""", """<a href="$0">$0</a>""")
+          } else null,
+          dom.select("#comments #comment-nav-below #comments-nav .next").headOption match {
+            case Some(a) => a.attr("abs:href")
+            case _ => null
+          },
+          dom.select("#comments .commentlist>li").map(e => new Comment(e)).toList
+        )
+      }
+
+      override def post(result: (String, String, List[Comment])): Unit = {
+        if (content) {
+          val web: WebView = findViewById(R.id.webview)
+          web.loadDataWithBaseURL(url, result._1, "text/html", "utf-8", null)
+        }
+        progress2.setTag(result._2)
+
+        adapter.data.append(result._3: _*)
+        adapter.notifyItemRangeInserted(adapter.data.size - result._3.size, result._3.size)
+        busy = false
+        busy2 = false
+      }
+    }.execute()
+  }
+
   override def onCreate(savedInstanceState: Bundle): Unit = {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_info)
     setSupportActionBar(findViewById(R.id.toolbar))
     setTitle(article.title)
 
+    val recycle: RecyclerView = findViewById(R.id.recycler)
     recycle.setLayoutManager(new LinearLayoutManager(this))
     recycle.setAdapter(adapter)
 
-    //    recycle.setOnScrollListener(new RecyclerView.OnScrollListener {
-    //      override def onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int): Unit =
-    //        super.onScrolled(recyclerView, dx, dy)
-    //
-    //      override def onScrollStateChanged(recyclerView: RecyclerView, newState: Int): Unit =
-    //        super.onScrollStateChanged(recyclerView, newState)
-    //    })
+    recycle.setOnScrollListener(new RecyclerView.OnScrollListener {
+      override def onScrolled(recycler: RecyclerView, dx: Int, dy: Int): Unit =
+        super.onScrolled(recycler, dx, dy)
 
-    busy(b = true)
-    new ScalaTask[Void, Void, (String, List[Comment])] {
-      override def background(params: Void*): (String, List[Comment]) = {
-        val http = new OkHttpClient()
-        val request = new Request.Builder().get().url(article.link).build()
-        val response = http.newCall(request).execute()
-        val html = response.body().string()
-        val dom = Jsoup.parse(html, article.link)
-        val content = dom.select(".entry-content")
-        val body = content.html().replaceAll( """\s{0,1}style=".*?"""", "")
-          .replaceAll( """\s{0,1}class=".*?"""", "")
-          .replaceAll( """<a href="#">.*?</a>""", "")
-          .replaceAll( """</?embed.*?>""", "")
-        //          .replaceAll( """(?<!\w+=['"]?)(?:https?://|magnet:\?xt=)[^\s\"\'\<]+""", """<a href="$0">$0</a>""")
-        Tuple2(
-          using(io.Source.fromInputStream(getResources.openRawResource(R.raw.template))) {
-            reader => reader.mkString.replace("{{title}}", article.title).replace("{{body}}", body)
-          },
-          dom.select("#comments .commentlist>li").map(e => new Comment(e)).toList
-        )
+      override def onScrollStateChanged(recycler: RecyclerView, state: Int): Unit = {
+        super.onScrollStateChanged(recycler, state)
+        (recycler.getLayoutManager, state) match {
+          case (linear: LinearLayoutManager, RecyclerView.SCROLL_STATE_IDLE) =>
+            (progress2.getTag, linear.findLastVisibleItemPosition()) match {
+              case (url: String, pos: Int) if !url.isEmpty && pos >= adapter.data.size - 2 => query(url)
+              case _ =>
+            }
+          case _ =>
+        }
       }
-
-      override def post(result: (String, List[Comment])): Unit = {
-        web.loadDataWithBaseURL(article.link, result._1, "text/html", "utf-8", null)
-        adapter.data.append(result._2: _*)
-        adapter.notifyDataSetChanged()
-        busy(b = false)
-      }
-    }.execute()
+    })
+    busy = false
+    busy2 = false
+    query(article.link, content = true)
   }
 }
