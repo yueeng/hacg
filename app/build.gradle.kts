5da71f3ecf46136ccf0289600b045d136c658f3c
Rain Yue <ssiori@gmail.com>
2020-12-23 16:49:58 +0800

    up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 android {
     compileSdkVersion(30)
-    buildToolsVersion = "30.0.2"
+    buildToolsVersion = "30.0.3"
 
     defaultConfig {
         applicationId = "io.github.yueeng.hacg"
@@ -63,7 +63,7 @@ dependencies {
     val okhttpVersion = "4.9.0"
     val kotlinxCoroutinesVersion = "1.4.1"
     implementation("androidx.appcompat:appcompat:1.3.0-alpha02")
-    implementation("com.google.android.material:material:1.3.0-alpha04")
+    implementation("com.google.android.material:material:1.3.0-beta01")
     implementation("androidx.preference:preference-ktx:1.1.1")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.recyclerview:recyclerview:1.2.0-beta01")
@@ -71,8 +71,8 @@ dependencies {
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.2.0-alpha01")
     implementation("androidx.viewpager2:viewpager2:1.1.0-alpha01")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-    implementation("androidx.fragment:fragment-ktx:1.3.0-beta01")
-    implementation("androidx.paging:paging-runtime-ktx:3.0.0-alpha09")
+    implementation("androidx.fragment:fragment-ktx:1.3.0-rc01")
+    implementation("androidx.paging:paging-runtime-ktx:3.0.0-alpha11")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinxCoroutinesVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$kotlinxCoroutinesVersion")
     implementation("com.github.clans:fab:1.6.4")
