f3fc94739112fb5c8d863e688cb49662305fcbd8
ssiori <ssiori@gmail.com>
2023-11-10 16:29:16 +0800

    AGP 8.1.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.2" apply false
+    id("com.android.application") version "8.1.3" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
-    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
+    id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
 }
