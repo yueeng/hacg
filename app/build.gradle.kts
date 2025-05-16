684428525f0a7590c72ee430288e9e790b37fe1d
rain <ssiori@gmail.com>
2025-05-16 16:47:43 +0800

    AGP 8.10.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,7 +66,7 @@ android {
 }
 
 dependencies {
-    val lifecycleVersion = "2.8.7"
+    val lifecycleVersion = "2.9.0"
     val glideVersion = "4.16.0"
     val okhttpVersion = "4.12.0"
     val kotlinxCoroutinesVersion = "1.10.2"
